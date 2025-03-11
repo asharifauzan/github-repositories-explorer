f898ff14218571f4946a12632614ca03e5576e4f
Fauzan Hibatullah Ashari <fauzansaja10@gmail.com>
2025-03-12 00:47:01 +0700

    chore: setup react testing library with jest

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from "vite"
+import { defineConfig, UserConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 // https://vite.dev/config/
@@ -10,5 +10,10 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src")
     }
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./tests/setup.ts"
   }
-})
+} as UserConfig)
